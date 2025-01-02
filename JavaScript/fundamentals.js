14061faa6bc949f02316d5a20a4dd4e7f29ecc06
Aditya <119723334+22MH1A42G1@users.noreply.github.com>
2025-01-02 11:52:50 +0530

    First Commit
    Basics of html, CSS, JS files

---
+++ JavaScript/fundamentals.js
@@ -0,0 +1,53 @@
+//console.log("Hello World! Aditya")
+
+// let a =2;
+// let b =3.14;
+// let c="aditya";
+// let d=true;
+// console.log(typeof(a))
+// console.log(typeof(b))
+// console.log(typeof(c))
+// console.log(typeof(d))
+
+// let a=2;
+// let b=String(a);
+// let c=Boolean(a);
+// let d ="abc";
+// let e="123";
+// let f = Number(d);
+// let g =Number(e);
+// console.log(typeof(b))
+// console.log(c)
+// console.log(f)
+// console.log(g)
+
+// let a = "Aditya";
+// let b = "Indana. ";
+// let c = b + a;
+// console.log(c);
+
+// let a = 2;
+// let b = 3;
+// console.log(a==b);
+
+// let a =2;
+// let b =1;
+// if(a==b){
+//     console.log("a is equal to b");
+// }
+// else if(a>b){
+//     console.log("a is greater to b");
+// }
+// else{
+//     console.log("a is not equal to b");
+// }
+
+// for (let index = 1; index <= 5; index++) {
+//     console.log(index);
+// }
+
+a =1;
+while (a<=5) {
+    console.log(a);
+    a++;
+}
